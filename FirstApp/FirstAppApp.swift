508dd9469337ebc082b6de23e3b7287656c51036
Cameron Bradshaw <cnbradshaw24@gmail.com>
2022-01-28 16:54:45 -0500

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Cameron Bradshaw on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
